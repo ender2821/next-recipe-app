03dafd346781c296a80afa645201b76564b4d1ee
Josh Jensen <JJensen@rightpoint.com>
2022-02-24 18:23:11 -0600

    fix build atempt 4

--- pages/api/add-favorite.js
+++ pages/api/add-favorite.js
@@ -1,12 +1,12 @@
-import { sanityClient } from "../../lib/sanity";
+import { sanityClientOld } from "../../lib/sanity";
 
-sanityClient.config ({
+sanityClientOld.config ({
   token: process.env.SANITY_WRITE_TOKEN,
 });
 
 export default async function favoriteButtonHandler(req, res) {
   const { _id } = JSON.parse(req.body);
-  const data = await sanityClient
+  const data = await sanityClientOld
     .patch(_id)
     .setIfMissing({ favorite: false })
     .set({ favorite: true })
