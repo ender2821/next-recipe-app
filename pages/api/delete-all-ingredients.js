b6977d8a776979ba25549f186361b8a09ae3e3a0
Joshua Jensen <ender2821@yahoo.com>
2023-08-15 19:57:10 -0600

    fix query attempt

--- pages/api/delete-all-ingredients.js
+++ pages/api/delete-all-ingredients.js
@@ -7,7 +7,7 @@ sanityClient.config ({
 export default async function favoriteButtonHandler(req, res) {
   const { _id, ingredientKeys } = JSON.parse(req.body);
 
-  const ingredientsToRemove = [`ingredient${ingredientKeys}`]
+  const ingredientsToRemove = [`ingredient[${ingredientKeys}]`]
 
   const data = await sanityClient.patch(_id).unset(ingredientsToRemove).commit().catch((error) => console.log(error))
   res.status(200).json({ data })
